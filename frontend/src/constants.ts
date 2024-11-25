05759b9ac5968d5e85215cb1cdf9b9eb51287eea
Alan Tai <alan@skellige.local>
2024-11-25 23:38:59 +0000

    First commit

---
+++ frontend/src/constants.ts
@@ -0,0 +1,11 @@
+import { theme, type ThemeConfig, } from 'antd';
+
+export const THEME : ThemeConfig = {
+    algorithm : theme.darkAlgorithm,
+    token     : {
+        colorPrimary  : '#fa8c16',
+        colorBgLayout : '#121212',
+    },
+};
+
+export const PAGE_WIDTH : number = 1200;
